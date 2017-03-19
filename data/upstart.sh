22618f6c7c6df5c77669eefcecd7ecbab6740c93
®•ابــو ســكروــب)•® <almansorr98@gmail.com>
2017-03-19 19:47:10 +0300

    Update upstart.sh

--- data/upstart.sh
+++ data/upstart.sh
@@ -1,1 +1,11 @@
-#!/bin/bashcopy_conf=`cp ./data/shieldtm.conf /etc/init/`copy_etc=`cp ./data/shieldtm.conf /etc/`reboot=`reboot`echo "$copy_conf"echo "$copy_etc"echo "$reboot"echo "Server Reboted" 
+#!/bin/bash
+
+
+copy_conf=`cp ./data/shieldtm.conf /etc/init/`
+copy_etc=`cp ./data/shieldtm.conf /etc/`
+reboot=`reboot`
+
+echo "$copy_conf"
+echo "$copy_etc"
+echo "$reboot"
+echo "Server Reboted" 
